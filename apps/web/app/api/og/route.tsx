c041ceb2287656d843c77aca833c9f67797cf1b1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-06 08:24:11 -0500

    Colors 🎨

--- apps/web/app/api/og/route.tsx
+++ apps/web/app/api/og/route.tsx
@@ -6,8 +6,11 @@ export async function GET() {
   const user = "Hacksore";
   const { data } = await getUserContributions({ user: user });
 
+  // all 52 weeks of contributions
   const weeklyContributions =
     data.user.contributionsCollection.contributionCalendar.weeks;
+
+  // all 7 days withing the week
   const contributionsPerWeek = weeklyContributions.map(
     (week) => week.contributionDays
   );
@@ -15,13 +18,16 @@ export async function GET() {
   return new ImageResponse(
     (
       <div tw="flex bg-black w-full h-full flex-row items-center justify-center">
-        <div tw="flex container mx-auto p-4">
+        <div tw="flex container">
           <div tw="flex grid grid-cols-7 gap-2">
             {contributionsPerWeek.map((week) =>
               week.map((day) => {
                 return (
-                  <div style={{ width: 16, height: 16, background: day.color }}/>
-                ) 
+                  <div
+                    key={day.date}
+                    style={{ width: 16, height: 16, background: day.color }}
+                  />
+                );
               })
             )}
           </div>
